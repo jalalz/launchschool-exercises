9a8b8bd23370901eb04f68a6e1838f3c587a6970
jalalz <jalalzarouil@gmail.com>
2016-12-04 13:18:25 +0100

    new exercises

---
+++ arrex4.rb
@@ -0,0 +1,9 @@
+#this won't run
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+puts arr.index(5) # shows which place the number, '5' in this case, has in the array
+
+# puts arr.index[5] # this doesn't exist
+
+puts arr[5] # this shows what is in the 5th spot of the array
---
+++ arrex7.rb
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4, 5]
+n_a = []
+
+a.each do |n|
+  new_arr << n + 2
+end
+
+p a
+p n_a 
